cd9a6d7c15218003ac6f02f0bc82587cf5bdb7af
Taras Drobotko <drobotkot@gmail.com>
2025-02-27 15:36:45 +0200

    load the plugin

--- flormar-test-slider.php
+++ flormar-test-slider.php
@@ -17,31 +17,104 @@ if (
     exit;
 }
 
-define('FL_TEST_SLIDER_VERSION', '1.0.0');
-define('FL_TEST_SLIDER_PLUGIN_PATH', plugin_dir_path(__FILE__));
-define('FL_TEST_SLIDER_PLUGIN_URL', plugin_dir_url(__FILE__));
+define('FL_SLIDER_VERSION', '1.0.0');
+define('FL_SLIDER_PLUGIN_PATH', plugin_dir_path(__FILE__));
+define('FL_SLIDER_PLUGIN_URL', plugin_dir_url(__FILE__));
 
 add_action('plugins_loaded', 'fl_init_slider_class');
 
 function fl_init_slider_class()
 {
-    include_once(FL_TEST_SLIDER_PLUGIN_PATH . 'class-fl-test-slider.php');
+    include_once(FL_SLIDER_PLUGIN_PATH . 'class-fl-test-slider.php');
 }
 
-add_action('wp_enqueue_scripts', 'fl_test_slider_enqueue_scripts');
-
-function fl_test_slider_enqueue_scripts()
+/**
+ * Check if WooCommerce plugin is active
+ */
+function fl_check_activation()
 {
-    wp_register_style('fl-slider-style', plugins_url('/assets/css/styles.css', __FILE__), false);
-    wp_enqueue_style('fl-slider-style');
+    if (! class_exists('WooCommerce')) {
+        if (is_plugin_active(plugin_basename(__FILE__))) {
+            // deactivate the plugin
+            deactivate_plugins(plugin_basename(__FILE__));
+
+            // unset activation notice
+            unset($_GET['activate']);
+
+            // display notice
+            add_action('admin_notices', 'fl_admin_notices');
+        }
+    }
 }
 
-add_action('wp_enqueue_scripts', 'fl_slick_register_styles');
 
-function fl_slick_register_styles()
+// Check required plugin is activated or not
+add_action('admin_init', 'fl_check_activation');
+
+/**
+ * Admin notices
+ */
+function fl_admin_notices()
 {
-    wp_enqueue_style('slick-css', plugins_url('/assets/src/library/css/slick.css', __FILE__), [], false, 'all');
-    wp_enqueue_style('slick-theme-css', plugins_url('/assets/src/library/css/slick-theme.css', __FILE__), ['slick-css'], false, 'all');
-    wp_enqueue_script('slick-js', plugins_url('/assets/src/library/js/slick.min.js', __FILE__), ['jquery'], false, true);
-    wp_enqueue_script('carousel-js', plugins_url('/assets/src/carousel/index.js', __FILE__), ['jquery', 'slick-js'], false, true);
+    if (! class_exists('WooCommerce')) {
+        echo '<div class="error notice is-dismissible">';
+
+        echo sprintf(__('<p><strong>%s</strong> requires the following plugin to be installed & activated.</p>', 'flormar-test-slider'), 'Flormar Test Slider');
+
+        echo sprintf(__('<p><strong><a href="%s" target="_blank">%s</a> </strong></p>', 'flormar-test-slider'), 'https://wordpress.org/plugins/woocommerce/', 'WooCommerce');
+
+        echo '</div>';
+    }
 }
+
+/**
+ * Load the plugin after the main plugin is loaded.
+ */
+function fl_load_plugin()
+{
+    // Check main plugin is active or not
+    if (class_exists('WooCommerce')) {
+        /**
+         * Add Go To Settings Page
+         */
+        function fl_goto_settings_page_link($links)
+        {
+            $goto_settings_link = array('<a href="' . admin_url('edit.php?post_type=product&page=flormar-test-slider') . '">' . __('Settings', 'flormar-test-slider') . '</a>');
+
+            return array_merge($links, $goto_settings_link);
+        }
+
+        add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'fl_goto_settings_page_link');
+
+        /**
+         * Load Text Domain
+         */
+        function fl_load_textdomain()
+        {
+            load_plugin_textdomain('flormar-test-slider', '', dirname(plugin_basename(__FILE__)) . '/languages/');
+        }
+
+        // Action to load plugin text domain
+        add_action('plugins_loaded', 'fl_load_textdomain');
+
+        add_action('wp_enqueue_scripts', 'fl_test_slider_enqueue_scripts');
+
+        /**
+         * Function to add frontend scripts and styles
+         */
+        function fl_test_slider_enqueue_scripts()
+        {
+            wp_enqueue_style('fl-slider-style', FL_SLIDER_PLUGIN_URL . 'assets/css/styles.css', array(), FL_SLIDER_VERSION);
+
+            wp_enqueue_style('slick-css', FL_SLIDER_PLUGIN_URL . 'assets/src/library/css/slick.css', [], FL_SLIDER_VERSION);
+            wp_enqueue_style('slick-theme-css', FL_SLIDER_PLUGIN_URL . 'assets/src/library/css/slick-theme.css', ['slick-css'], FL_SLIDER_VERSION);
+            wp_enqueue_script('slick-js', FL_SLIDER_PLUGIN_URL . 'assets/src/library/js/slick.min.js', ['jquery'], FL_SLIDER_VERSION, true);
+            wp_enqueue_script('carousel-js', FL_SLIDER_PLUGIN_URL . 'assets/src/carousel/index.js', ['jquery', 'slick-js'], FL_SLIDER_VERSION, true);
+        }
+
+        add_action('wp_enqueue_scripts', 'fl_test_slider_enqueue_scripts');
+    }
+}
+
+// Action to load plugin after the main plugin is loaded
+add_action('plugins_loaded', 'fl_load_plugin', 15);
